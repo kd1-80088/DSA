5905fb14359bd4e3218891b44f193f6906c25dbc
kd1-80088 <pranavbandgar55@gmail.com>
2023-11-02 23:19:05 +0530

    Assign2

--- Assignment_2/Q8/src/com/sunbeam/CircularQueue.java
+++ Assignment_2/Q8/src/com/sunbeam/CircularQueue.java
@@ -1,62 +1,43 @@
 package com.sunbeam;
 
 public class CircularQueue {
-	private int front;
-	private int rear;
-	private int arr[];
-	private int size;
+	private int rear, front;
+	private final int SIZE;
+	private int[] arr;
+	private int count = 0;
 
 	public CircularQueue(int size) {
-		this.size = size;
-		this.arr = new int[size];
-		this.front = -1;
-		this.rear = -1;
+		SIZE = size;
+		rear = -1;
+		front = -1;
+		arr = new int[SIZE];
 	}
 
 	public void push(int data) {
-		if (rear == size - 1) {
-			rear = 0;
-		} else {
-			rear++;
-		}
-
+		count++;
+		rear = (rear + 1) % SIZE;
 		arr[rear] = data;
-		if (front == -1)
-			front++;
 	}
 
 	public void pop() {
-
-		arr[front] = -1;
-		if (front == rear) {
-			rear = -1;
-			front = -1;
-		} else
-
-		{
-			if (front == size - 1) {
-				front = 0;
-			} else
-				front++;
-		}
+		count--;
+		front = (front + 1) % SIZE;
+		if (front == rear)
+			front = rear = -1;
 
 	}
 
 	public int peek() {
-		return arr[front];
+		return arr[(front + 1) % SIZE];
+
 	}
 
 	public boolean isFull() {
-		if ((rear == size - 1 && front == 0) || (rear + 1 == front))
-			return true;
-		else
-			return false;
+		return count == SIZE;
 	}
 
 	public boolean isEmpty() {
-		if (rear == -1) {
-			return true;
-		}
-		return false;
+		return count == 0;
 	}
+
 }
